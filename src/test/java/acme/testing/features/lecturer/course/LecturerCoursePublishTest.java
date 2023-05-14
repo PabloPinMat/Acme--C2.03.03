79ad555245a52e66b1a0b4ed5e4d3acd3287cb9e
pablo <pablo@DESKTOP-OLJ3UGL>
2023-05-14 17:10:29 +0200

    New test added

--- src/test/java/acme/testing/features/lecturer/course/LecturerCoursePublishTest.java
+++ src/test/java/acme/testing/features/lecturer/course/LecturerCoursePublishTest.java
@@ -10,17 +10,36 @@ public class LecturerCoursePublishTest extends TestHarness {
 
 	@ParameterizedTest
 	@CsvFileSource(resources = "/features/lecturer/course/publish-positive.csv", encoding = "utf-8", numLinesToSkip = 1)
-	public void test100Positive(final int recordIndex) {
+	public void test100Positive(final int courseIndex) {
 
 		super.signIn("lecturer1", "lecturer1");
 		super.clickOnMenu("Teacher", "List Courses");
 		super.checkListingExists();
 		super.sortListing(0, "asc");
 
-		super.clickOnListingRecord(recordIndex);
+		super.clickOnListingRecord(courseIndex);
 		super.checkFormExists();
 		super.clickOnSubmit("Publish");
 		super.checkNotErrorsExist();
+
+		super.signOut();
+	}
+
+	@ParameterizedTest
+	@CsvFileSource(resources = "/features/lecturer/course/publish-negative.csv", encoding = "utf-8", numLinesToSkip = 1)
+	public void test200Negative(final int courseIndex) {
+
+		super.signIn("lecturer2", "lecturer2");
+
+		super.clickOnMenu("Teacher", "List Courses");
+		super.checkListingExists();
+		super.sortListing(0, "asc");
+
+		super.clickOnListingRecord(courseIndex);
+		super.checkFormExists();
+		super.clickOnSubmit("Publish");
+
+		super.checkErrorsExist();
 
 		super.signOut();
 	}
