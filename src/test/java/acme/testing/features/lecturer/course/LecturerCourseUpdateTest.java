35adbb2c6e53055db069fa1aab860a7fb894e0bb
pablo <pablo@DESKTOP-OLJ3UGL>
2023-05-14 19:35:05 +0200

    Update Test added

--- src/test/java/acme/testing/features/lecturer/course/LecturerCourseUpdateTest.java
+++ src/test/java/acme/testing/features/lecturer/course/LecturerCourseUpdateTest.java
@@ -1,6 +1,71 @@
+
 package acme.testing.features.lecturer.course;
 
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvFileSource;
 
-public class LecturerCourseUpdateTest {
+import acme.testing.TestHarness;
 
+public class LecturerCourseUpdateTest extends TestHarness {
+
+	@ParameterizedTest
+	@CsvFileSource(resources = "/features/lecturer/course/update-positive.csv", encoding = "utf-8", numLinesToSkip = 1)
+	public void test100Positive(final int courseIndex, final String code, final String title, final String courseAbstract, final String retailPrice, final String link) {
+		// HINT: this test logs in as an employer, lists his or her jobs, 
+		// HINT+ selects one of them, updates it, and then checks that 
+		// HINT+ the update has actually been performed.
+
+		super.signIn("lecturer2", "lecturer2");
+
+		super.clickOnMenu("Teacher", "List Courses");
+		super.checkListingExists();
+		super.sortListing(0, "asc");
+
+		super.clickOnListingRecord(courseIndex);
+		super.checkFormExists();
+		super.fillInputBoxIn("code", code);
+		super.fillInputBoxIn("title", title);
+		super.fillInputBoxIn("courseAbstract", courseAbstract);
+		super.fillInputBoxIn("retailPrice", retailPrice);
+		super.fillInputBoxIn("link", link);
+		super.clickOnSubmit("Update");
+
+		super.checkListingExists();
+		super.sortListing(0, "asc");
+
+		super.clickOnListingRecord(courseIndex);
+		super.checkFormExists();
+		super.fillInputBoxIn("code", code);
+		super.fillInputBoxIn("title", title);
+		super.fillInputBoxIn("courseAbstract", courseAbstract);
+		super.fillInputBoxIn("retailPrice", retailPrice);
+		super.fillInputBoxIn("link", link);
+
+		super.signOut();
+	}
+
+	@ParameterizedTest
+	@CsvFileSource(resources = "/features/lecturer/course/update-negative.csv", encoding = "utf-8", numLinesToSkip = 1)
+	public void test100Negative(final int courseIndex, final String code, final String title, final String courseAbstract, final String retailPrice, final String link) {
+
+		super.signIn("lecturer2", "lecturer2");
+
+		super.clickOnMenu("Teacher", "List Courses");
+
+		super.checkListingExists();
+
+		super.clickOnListingRecord(courseIndex);
+
+		super.fillInputBoxIn("code", code);
+		super.fillInputBoxIn("title", title);
+		super.fillInputBoxIn("courseAbstract", courseAbstract);
+		super.fillInputBoxIn("retailPrice", retailPrice);
+		super.fillInputBoxIn("link", link);
+
+		super.clickOnSubmit("Update");
+
+		super.checkErrorsExist();
+
+		super.signOut();
+	}
 }
