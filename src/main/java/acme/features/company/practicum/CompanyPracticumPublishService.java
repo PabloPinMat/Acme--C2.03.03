f32638eee95112cbf98b5d35219bf61db3588f76
rauulhernaan <raumerbas@alum.us.es>
2023-05-26 21:49:47 +0200

    Fix: Pequeños errores solucionados

--- src/main/java/acme/features/company/practicum/CompanyPracticumPublishService.java
+++ src/main/java/acme/features/company/practicum/CompanyPracticumPublishService.java
@@ -74,6 +74,13 @@ public class CompanyPracticumPublishService extends AbstractService<Company, Pra
 	@Override
 	public void validate(final Practicum object) {
 		assert object != null;
+
+		if (!super.getBuffer().getErrors().hasErrors("code"))
+			super.state(this.repository.findPracticumByCode(object.getCode()) == null || this.repository.findPracticumByCode(object.getCode()).equals(object), "code", "company.practicum.form.error.code");
+
+		if (!super.getBuffer().getErrors().hasErrors("course"))
+			super.state(!object.getCourse().isDraftMode(), "course", "company.practicum.form.error.code2");
+
 	}
 
 	@Override
