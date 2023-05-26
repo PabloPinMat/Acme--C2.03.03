cd288fa902cea47f897c68f526fd1fe1ab226abb
ferbaqfer <ferbaqfer@alum.us.es>
2023-05-26 19:32:15 +0200

    fix: Correccion update v2

--- src/test/java/acme/testing/features/student/enrolment/StudentEnrolmentUpdateTest.java
+++ src/test/java/acme/testing/features/student/enrolment/StudentEnrolmentUpdateTest.java
@@ -15,36 +15,36 @@ public class StudentEnrolmentUpdateTest extends TestHarness {
 
 	// Test methods -----------------------------------------------------------
 
-	//	@ParameterizedTest
-	//	@CsvFileSource(resources = "/features/student/enrolment/update-positive.csv", encoding = "utf-8", numLinesToSkip = 1)
-	//	public void test100Positive(final int enrolmentIndex, final String code, final String course, final String motivation, final String goals) {
-	//		// HINT: this test authenticates as an employer, lists his or her applications,
-	//		// HINT+ changes their status and checks that it's been updated.
-	//
-	//		super.signIn("student2", "student2");
-	//
-	//		super.clickOnMenu("Student", "My enrolments");
-	//
-	//		super.checkListingExists();
-	//
-	//		super.clickOnListingRecord(enrolmentIndex);
-	//
-	//		super.checkFormExists();
-	//
-	//		super.fillInputBoxIn("code", code);
-	//		super.fillInputBoxIn("course", course);
-	//		super.fillInputBoxIn("motivation", motivation);
-	//		super.fillInputBoxIn("goals", goals);
-	//
-	//		super.clickOnSubmit("Update");
-	//
-	//		super.checkListingExists();
-	//
-	//		super.checkColumnHasValue(0, 0, code);
-	//
-	//		super.signOut();
-	//	}
 
+	@ParameterizedTest
+	@CsvFileSource(resources = "/features/student/enrolment/update-positive.csv", encoding = "utf-8", numLinesToSkip = 1)
+	public void test100Positive(final int enrolmentIndex, final String code, final String course, final String motivation, final String goals) {
+		// HINT: this test authenticates as an employer, lists his or her applications,
+		// HINT+ changes their status and checks that it's been updated.
+
+		super.signIn("student2", "student2");
+
+		super.clickOnMenu("Student", "My enrolments");
+
+		super.checkListingExists();
+
+		super.clickOnListingRecord(enrolmentIndex);
+
+		super.checkFormExists();
+
+		super.fillInputBoxIn("code", code);
+		super.fillInputBoxIn("course", course);
+		super.fillInputBoxIn("motivation", motivation);
+		super.fillInputBoxIn("goals", goals);
+
+		super.clickOnSubmit("Update");
+
+		super.checkListingExists();
+
+		super.checkColumnHasValue(0, 0, code);
+
+		super.signOut();
+	}
 
 	@ParameterizedTest
 	@CsvFileSource(resources = "/features/student/enrolment/update-negative.csv", encoding = "utf-8", numLinesToSkip = 1)
@@ -72,8 +72,6 @@ public class StudentEnrolmentUpdateTest extends TestHarness {
 
 	@Test
 	public void test300Hacking() {
-		// HINT: this test tries to update an enrolment with a role other than "Student",
-		// HINT+ or using an student who is not the owner.
 
 		super.checkLinkExists("Sign in");
 		super.request("/student/enrolment/update", "24");
